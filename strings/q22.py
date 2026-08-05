388dba6c236480c440a50b49024a4b7f4c33e698
Snehal2862007 <snehaldalvi9560@gmail.com>
2026-08-05 20:49:18 +0530

    old

---
+++ strings/q22.py
@@ -0,0 +1,14 @@
+def run_length_encoding(s):
+    if not s:
+        return ""
+    result = []
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            result.append(f"{s[i-1]}{count}")
+            count = 1
+    result.append(f"{s[-1]}{count}")
+    return "".join(result)
+print(run_length_encoding("aaabbccccd"))
